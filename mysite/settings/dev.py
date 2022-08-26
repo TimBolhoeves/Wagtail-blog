e0cdaccb9be08b0b9f648b5c89a3f900e91a866e
TimBolhoeve <tim.bolhoeve@deboer-group.com>
2022-08-26 11:54:36 +0200

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-oud+!-uuiy)8m!0_^=wb%pydho&(8i1a&d@9cte1+(_cn_ki!o"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
